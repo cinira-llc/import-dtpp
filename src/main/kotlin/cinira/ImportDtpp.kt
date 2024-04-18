7c235348ded420b60b38f0cf2e916abbe5d4cb9c
Matt Jensen <mattj65816@gmail.com>
2024-04-17 20:09:35 -0500

    Delete drop after import.

--- src/main/kotlin/cinira/ImportDtpp.kt
+++ src/main/kotlin/cinira/ImportDtpp.kt
@@ -10,20 +10,32 @@ import java.lang.System.getenv
 import java.net.URI
 
 @Suppress("unused")
-class ImportDtpp : RequestHandler<S3Event, List<URI>> {
-    private val client = S3Client.create()
+class ImportDtpp internal constructor(
+    private val client: S3Client,
+    private val importer: ImportDtppSegment
+) : RequestHandler<S3Event, List<URI>> {
+    constructor() : this(
+        client = S3Client.create(),
+        targetBucket = getenv("TARGET_BUCKET")
+    )
+
+    internal constructor(client: S3Client, targetBucket: String) : this(
+        client = client,
+        importer = ImportDtppSegmentImpl(
+            client = client,
+            targetBucket = targetBucket
+        )
+    )
 
     override fun handleRequest(event: S3Event, ctx: Context): List<URI> {
-        val importer = ImportDtppSegmentImpl(
-            client = client,
-            targetBucket = getenv("TARGET_BUCKET")
-        )
         return event.records.map { record ->
             val bucket = record.s3.bucket.name
             val key = record.s3.`object`.key
             log.debug("Processing dTPP segment at bucket [{}] key [{}].", bucket, key)
             val resource = client.get(bucket, key)
-            importer.execute(resource)
+            importer.execute(resource).also {
+                client.deleteObject { delete -> delete.bucket(bucket).key(key) }
+            }
         }.toList()
     }
 
